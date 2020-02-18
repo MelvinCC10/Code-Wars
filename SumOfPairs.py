8c2a88b2b3606c8db0412531f0dfeb872b8b8603
Melvin Cordon <41267715+MelvinCC10@users.noreply.github.com>
2020-02-17 22:07:33 -0600

    Create SumOfPairs.py

---
+++ SumOfPairs.py
@@ -0,0 +1,37 @@
+"""
+sum_pairs([11, 3, 7, 5],         10)
+#              ^--^      3 + 7 = 10
+== [3, 7]
+
+sum_pairs([4, 3, 2, 3, 4],         6)
+#          ^-----^         4 + 2 = 6, indices: 0, 2 *
+#             ^-----^      3 + 3 = 6, indices: 1, 3
+#                ^-----^   2 + 4 = 6, indices: 2, 4
+#  * entire pair is earlier, and therefore is the correct answer
+== [4, 2]
+
+sum_pairs([0, 0, -2, 3], 2)
+#  there are no pairs of values that can be added to produce 2.
+== None/nil/undefined (Based on the language)
+
+sum_pairs([10, 5, 2, 3, 7, 5],         10)
+#              ^-----------^   5 + 5 = 10, indices: 1, 5
+#                    ^--^      3 + 7 = 10, indices: 3, 4 *
+#  * entire pair is earlier, and therefore is the correct answer
+== [3, 7]
+
+"""
+
+def sum_pairs(nums, target):
+
+    num_index = {}
+    
+    for i, num in enumerate(nums):
+        left_over = target - (num)
+        
+        if left_over not in num_index:
+            num_index[num] = i
+        else:
+            return [left_over,num]
+            
+ 
